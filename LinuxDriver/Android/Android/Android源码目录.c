86609a5bb62918d40cb6fbf042ca32c4ea3e0a1e
JamesHarden24 <410212660@qq.com>
2017-05-06 13:22:10 +0800

    2017-5-6 刷新学习笔记

---
+++ LinuxDriver/Android/Android/Android源码目录.c
@@ -0,0 +1,32 @@
+/*******************************************************************************
+Android 源码目录              描  述
+  abi            abi相关代码，abi:application binary interface应用程序二进制接口
+  art            全新的运行环境，需要和Dalvik VM分开
+ bionic          bionic C库
+ bootable        启动引导相关代码
+ build           存放系统编译规则及generic等基础开发配置包
+  cts            Android兼容性测试套件标准
+ dalvik          dalvik Java虚拟机
+ development     应用程序开发相关
+ device          设备相关代码
+ docs            介绍开元的相关文档
+ external        Android使用的一些开元的模组
+ frameworks      核心架构--Java以及C++语言，是Android应用程序的框架
+   gdk           即时通信模块
+ hardware        主要是硬件适配层HAL代码
+  kernel         Linux的内核文件
+ libcore         核心库相关
+ libnativehelper 是Support functions for Android's class libraries的缩写，表示动
+                 态库，是实现JNI库的基础
+   ndk           ndk相关代码。Android NDK是一系列的开发工具，允许程序开发人员在
+                 Android应用程序中嵌入C/C++语言编写的非托管代码。
+   out           编译完成后的代码输出在此目录
+  packages       应用程序包
+   pdk           Plug Development Kit的缩写，是本地开发套件
+  prebuilts      x86和arm架构下预编译的一些资源
+   sdk           sdk以及模拟器
+  system         文件系统和应用以及组件，使用C语言实现的
+  tools          工具文件夹
+  vendor         厂商定制代码
+  Makefile       全局Makefile 
+*******************************************************************************/
