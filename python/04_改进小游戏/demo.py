7e4c7199f6d50c26c5b32f2628ac8116d3a0eeba
JamesHarden24 <410212660@qq.com>
2017-06-19 06:14:40 +0800

    [笔记刷新]

--- python/04_改进小游戏/demo.py
+++ python/04_改进小游戏/demo.py
@@ -1,13 +1,20 @@
-print('-----------------------------------------------')
-while guess != 8
+import random
+
+secrect = random.randint(1, 10)
+
+print('------------Rookie.com-------------------')
+num = 3  #每个人有三次机会
+while num > 0:  
     temp = input("不妨猜一下小甲鱼现在心里想的是哪个数字:")
     guess = int(temp)
-    if guess == 8:
+    if guess == secrect:
         print("我艹，你是小甲鱼心里的蛔虫吗？！")
         print("哼，猜中了也没有奖励！")
+        num = 0
     else:
-        if guess > 8:
+        if guess > secrect:
             print('哥，大了，大了')
         else:
             print('嘿，小了，小了')
+        num = num - 1
 print("游戏结束，不玩了^_^")
---
+++ python/06_常用操作符/demo.py
@@ -0,0 +1,21 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 18:41:36) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> a = 3
+>>> a += 3
+>>> a
+6
+>>> a = b = c = d = 5
+>>> a
+5
+>>> b
+5
+>>> c
+5
+>>> a = 3 ** 2
+>>> a
+9
+>>> 3 ** -1
+0.3333333333333333
+>>> 3  ** -2
+0.1111111111111111
+>>> 
