ac574eb53ad9dbcd71807b1fcdb0c3bfd67e1b94
JamesHarden24 <410212660@qq.com>
2017-06-25 12:12:42 +0800

    【刷新笔记】

---
+++ python/11_字典/demo.py
@@ -0,0 +1,5 @@
+>>> dictl = {'李宁':'一切皆有可能', 'Nike':'Just do it', 'Adidas':'Impossible is noting', 'Rookie':'I can'}
+>>> print('Rookie 的口号是', dictl[Rookie])
+>>> print('Rookie 的口号是', dictl['Rookie'])
+Rookie 的口号是 I can
+>>> 
